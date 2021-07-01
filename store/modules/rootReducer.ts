73c7c33d5382ac8bd8e816a003abbdcbed4c55b2
Fabrício J. Silva <fabriciosilva@rvdigitalbrasil.com.br>
2021-07-01 09:47:39 -0300

    Criado a listagem de categoria

--- store/modules/rootReducer.ts
+++ store/modules/rootReducer.ts
@@ -1,6 +1,10 @@
 import { combineReducers } from "redux";
 import auth from './auth/reducer';
+import category from './admin/category/reducer';
+import search from './admin/shared/search/reducer';
 
 export default combineReducers({
-    auth
+    auth,
+    category,
+    search
 });
