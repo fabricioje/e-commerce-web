280508d5228d5d1f95fd670bba78daf679e9a81a
Fabrício J. Silva <fabriciosilva@rvdigitalbrasil.com.br>
2021-07-02 09:25:28 -0300

    Implementado a listagem de produtos

--- store/modules/rootReducer.ts
+++ store/modules/rootReducer.ts
@@ -2,9 +2,11 @@ import { combineReducers } from "redux";
 import auth from './auth/reducer';
 import category from './admin/category/reducer';
 import search from './admin/shared/search/reducer';
+import product from './admin/product/reducer';
 
 export default combineReducers({
     auth,
     category,
+    product,
     search
 });
