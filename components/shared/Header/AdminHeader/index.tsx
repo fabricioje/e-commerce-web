81f5341ded130e09ab32675472db77b4618ab4f2
Fabrício J. Silva <fabriciosilva@rvdigitalbrasil.com.br>
2021-07-04 17:03:04 -0300

    Adicionando SignOut, corrigindo placeholder da pesquisa e adicionando featured no produto

--- components/shared/Header/AdminHeader/index.tsx
+++ components/shared/Header/AdminHeader/index.tsx
@@ -3,16 +3,22 @@ import { Row, Col } from 'react-bootstrap';
 // import styles from '../../../../styles/AdminHeader.module.css';
 import styles from './styles.module.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSignal, faUser, faGamepad, faCheckSquare, faLaptop, faTicketAlt, faDollarSign, faUserCircle } from '@fortawesome/free-solid-svg-icons';
+import { faSignal, faUser, faGamepad, faCheckSquare, faLaptop, faTicketAlt, faDollarSign, faUserCircle, faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
+
+import SignOutService from '../../../../util/SignOutService';
+
+import { useSelector } from 'react-redux';
+import User from '../../../../dtos/User';
 
 interface AdminHeaderProps {
     name: string
 }
 
-const AdminHeader: React.FC<AdminHeaderProps> = ({ name }) => {
+const AdminHeader: React.FC = () => {
     const router = useRouter();
+    const { name }: User = useSelector(state => state.auth.loggedUser);
 
     return (
         <Row className={styles.background}>
@@ -24,6 +30,20 @@ const AdminHeader: React.FC<AdminHeaderProps> = ({ name }) => {
                 <Link href="/Admin/SystemRequirements/List"><a><FontAwesomeIcon icon={faLaptop} color="var(--color-gray-light)" className={`ml-3 ${router.pathname === '/Admin/SystemRequirements/List' ? styles.active : ''}`} /></a></Link>
                 <Link href="/Admin/Coupons/List"><a><FontAwesomeIcon icon={faTicketAlt} color="var(--color-gray-light)" className={`ml-3 ${router.pathname === '/Admin/Coupons/List' ? styles.active : ''}`} /></a></Link>
                 <Link href="#"><a><FontAwesomeIcon icon={faDollarSign} color="var(--color-gray-light)" className="ml-3" /></a></Link>
+
+                <Link href="/Auth/Login" >
+                    <a
+                        onClick={SignOutService.execute}
+                        // mobile don't have click so we need to use onTouch events
+                        onTouchEnd={() => SignOutService.execute()}
+                    >
+                        <FontAwesomeIcon
+                            icon={faSignOutAlt}
+                            color="var(--color-gray-light)"
+                            className="ml-3"
+                        />
+                    </a>
+                </Link>
             </Col>
 
             <Col lg={6} xs={3} className={styles.profile}>
--- components/shared/StyledButton/index.tsx
+++ components/shared/StyledButton/index.tsx
@@ -4,19 +4,24 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { IconProp } from '@fortawesome/fontawesome-svg-core';
 import styles from './styles.module.css';
 
-type ButtonProps =  ButtonHTMLAttributes<HTMLButtonElement> & {
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
     icon?: IconProp;
     action?: string;
     type_button: string;
+    active?: boolean;
 }
 
-const StyledButton: React.FC<ButtonProps> = ({icon, action, type_button, ...rest}) => {
+const StyledButton: React.FC<ButtonProps> = ({ icon, action, type_button, active = false, ...rest }) => {
     return (
-        <Button 
-            className={(type_button == "red") ? styles.red_button : styles.blue_button}
+        <Button
+            className={`
+                ${(type_button == "red") ? styles.red_button : styles.blue_button} 
+                ${active ? 'active' : ''}
+                `
+            }
             {...rest}
         >
-            { icon && <FontAwesomeIcon icon={icon} className={action && "mr-2"} /> } {action}
+            {icon && <FontAwesomeIcon icon={icon} className={action && "mr-2"} />} {action}
         </Button>
     )
 }
